1ecc641ca8b746872986c2fc5bbf679cb0e7c681
Dominic Jack <thedomjack@gmail.com>
2018-07-16 07:32:17 +1000

    removed vis imports to speed up import

--- skeleton/__init__.py
+++ skeleton/__init__.py
@@ -1,16 +1,9 @@
 from base import Skeleton, skeleton_height, front_angle
 from converters import SkeletonConverter
-from vis import vis2d, vis3d, default_ax2d, default_ax3d, rescale_ax3d
 
 __all__ = [
     Skeleton,
     SkeletonConverter,
     skeleton_height,
     front_angle,
-
-    vis2d,
-    vis3d,
-    default_ax2d,
-    default_ax3d,
-    rescale_ax3d,
 ]
